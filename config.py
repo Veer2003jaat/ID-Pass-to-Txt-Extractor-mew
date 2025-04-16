dcd87cbc324f588c433d7b0ce1cd50efa0b8d338
काला बिच्छू 🦂 <jaiveer2003godara@gmail.com>
2025-04-16 08:05:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 api_id = 28528007
 api_hash = "38464da16c80310cabc8d13952419cf3"
-bot_token = "7001245667:AAGm3VkNigvYgO5nrB9Civflz9lop0B91d8"
-auth_users = "6329158981"
+bot_token = "7791600471:AAHX-e9fsKMfh-Xzo0hoU3V0JdP6p0OBCX0"
+auth_users = "7414415335"
 sudo_users = [int(num) for num in auth_users.split(",")]
-osowner_users = "6329158981"
+osowner_users = "7414415335"
 owner_users = [int(num) for num in osowner_users.split(",")]
